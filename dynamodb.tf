151c360d08059f3349f771a4a757088261d3e54a
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-24 20:55:37 -0400

    Updated the dynamodb table.

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,11 +1,23 @@
 resource "aws_dynamodb_table" "tf_notes_table" {
   name           = "tf-notes-table"
   billing_mode   = "PROVISIONED"
-  read_capacity  = "30"
-  write_capacity = "30"
+  read_capacity  = 30
+  write_capacity = 30
   attribute {
     name = "noteId"
     type = "S"
   }
   hash_key = "noteId"
+  ttl {
+    enabled        = true
+    attribute_name = "expiryPeriod"
+  }
+  point_in_time_recovery { enabled = true }
+  server_side_encryption { enabled = true }
+  lifecycle { ignore_changes = [write_capacity, read_capacity] }
+}
+
+module "table_autoscaling" {
+  source     = "snowplow-devops/dynamodb-autoscaling/aws"
+  table_name = aws_dynamodb_table.tf_notes_table.name
 }
