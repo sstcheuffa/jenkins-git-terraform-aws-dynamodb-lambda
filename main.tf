5ed4866fc54e3a2e58391f81562aa6fb68cd7d9c
sstcheuffa <113473683+sstcheuffa@users.noreply.github.com>
2022-11-05 22:59:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-aws-jenkins-git-lambda-dynamodb-s3-state-backend"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_state"
+ # backend "s3" {
+ #   bucket         = "terraform-aws-jenkins-git-lambda-dynamodb-s3-state-backend"
+ #   key            = "terraform.tfstate"
+ #   region         = "us-east-1"
+ #   dynamodb_table = "terraform_state"
   }
 
   required_providers {
